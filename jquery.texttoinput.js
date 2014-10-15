efd3e5e97d7fe7275f19572ea8ba203ddbc8b856
Nico Puhlmann <nico@puhlmann.com>
2014-10-15 19:23:51 +0200

    onChange callback

--- jquery.texttoinput.js
+++ jquery.texttoinput.js
@@ -32,32 +32,61 @@
 
         /* browser supports contenteditable */
         if(hasContentEditable) {
+
           select($(this));
-          return;
+          input_element = $(this);
+          
+        } else {
+
+          e.preventDefault();
+          e.stopPropagation();
+
+          var text = $(this).text(),
+              default_html = $(this).html(),
+              input_html = '<input value="' + text + '" class="' + settings.css_class + '">',
+              input_element = $(this).html( input_html ).find('input').first();
+
+          $(this).data('default_html', default_html).unbind('click.texttoinput');
+
+          select(input_element);
+          
         }
 
-        e.preventDefault();
-        e.stopPropagation();
-
-        var text = $(this).text(),
-            default_html = $(this).html(),
-            input_html = '<input value="' + text + '" class="' + settings.css_class + '">',
-            input_element = $(this).html( input_html ).find('input').first();
-
-        $(this).data('default_html', default_html).unbind('click.texttoinput');
-
-        select(input_element);
 
         input_element.bind('blur.texttoinput', function() {          
           $(this).unbind('blur.texttoinput');
-          $(this).parent().html( $(this).parent().data('default_html') ).bind('click.texttoinput', toggle);
+          change($(this));
+          if(!hasContentEditable) {
+            $(this).parent().html( $(this).parent().data('default_html') ).bind('click.texttoinput', toggle);
+          }
         });
+        
+        if(settings.return_change === true) {
+          input_element.bind('keydown.texttoinput', function(e) {
+            e.preventDefault();
+            $(this).unbind('keydown.texttoinput');
+            if(e.which == 13) {
+              $(this).blur();
+            }
+          });
+          }
 
       },
 
+      change = function(el) {
+      
+        if(typeof(settings.onChange) == 'function') {
+          if(hasContentEditable) {
+            el.val(el.text());
+          }
+          settings.onChange.apply(el[0], arguments)
+        }
+      },
+      
       select = function (el) {
 
         if(settings.select_text !== true) {
+          el.focus().val(el.val());
           return el;
         }
 
@@ -96,7 +125,9 @@
   $.fn.textToInput.defaults = {
     select_text: true,     // whenever the input field should be selected with focus
     css_class: '',         // css class of input element
-    contenteditable: true  // enable or disable contenteditable support
+    contenteditable: true, // enable or disable contenteditable support
+    onChange: null,        // onChange callback 
+    return_change: true    // whenever onChange should be invoked when return key is pressed
   };
   
   $(function () {
